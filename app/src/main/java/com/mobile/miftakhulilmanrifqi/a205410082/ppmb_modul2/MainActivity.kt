40c837c0b7c48952ff33628898104fe5a6580d6f
Miftakhul-Ilman-Rifqi <bozzclax@gmail.com>
2023-07-31 23:31:26 +0700

    first commit

---
+++ app/src/main/java/com/mobile/miftakhulilmanrifqi/a205410082/ppmb_modul2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.miftakhulilmanrifqi.a205410082.ppmb_modul2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
